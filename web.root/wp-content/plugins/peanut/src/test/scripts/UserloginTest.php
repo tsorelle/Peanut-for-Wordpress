8311dcc867382fac9ee6c673e082720e2e038cf7
Terry SoRelle <tls@2quakers.net>
2017-12-24 07:07:49 -0600

    retest user->signIn

--- web.root/wp-content/plugins/peanut/src/test/scripts/UserloginTest.php
+++ web.root/wp-content/plugins/peanut/src/test/scripts/UserloginTest.php
@@ -17,7 +17,22 @@ class UserloginTest extends TestScript
     public function execute()
     {
         $username = 'ScriptMasterKF89289';
-        $actual = TUser::SignIn('ScriptMasterKF89289','M6tJb@1*yoIf97cFCQlDFmwJ');
+        if (TUser::getCurrent()->isAuthenticated()) {
+            exit('Log out before running test');
+        }
+        /*
+                password ignored in Drupal
+                $actual = TUser::SignIn($username,'bad password');
+                $this->assert($actual === false,'Sign in result should have been false');
+        */
+
+        $actual = TUser::SignIn('unknownuser','bad password');
+        $this->assert($actual === false,'Sign in result should have been false');
+
+        $expected = !TUser::getCurrent()->isAuthenticated();
+        $this->assert($expected,'Expected anonymous');
+
+        $actual = TUser::SignIn($username,'M6tJb@1*yoIf97cFCQlDFmwJ');
         $this->assert($actual,'Not logged in');
         $actual = TUser::getCurrent()->getUserName();
         $this->assertEquals($username,$actual,'Wrong user is current');
